c602477f1a804aba4bf7616bc0160ed67d656b65
AlexandraPauna <alexandrapauna927@yahoo.com>
2018-12-19 14:58:28 +0200

    Add project files.

---
+++ DAWProiect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DAWProiect.MvcApplication" Language="C#" %>
---
+++ DAWProiect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DAWProiect.Startup))]
+namespace DAWProiect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
