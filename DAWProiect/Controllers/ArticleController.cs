e026842d3eb20132322b43b21c76af5db76b71f5
MirunaRux <miruna_ruxandra97@yahoo.com>
2018-12-26 17:28:26 +0200

    ArticleController Added

---
+++ DAWProiect/Controllers/ArticleController.cs
@@ -0,0 +1,151 @@
+﻿using DAWProiect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAWProiect.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            // var articles = from article in db.Articles
+            //               select article;
+            // Solutia: Eager loading
+            var articles = db.Articles.Include("Category");
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.message = TempData["message"].ToString();
+            }
+            ViewBag.Articles = articles;
+
+            return View();
+        }
+
+
+        public ActionResult Show(int id)
+        {
+            Article article = db.Articles.Find(id);
+            ViewBag.Article = article;
+            ViewBag.Category = article.Category;
+
+            return View(article);
+
+        }
+
+        public ActionResult New()
+        {
+            Article article = new Article();
+            // preluam lista de categorii din metoda GetAllCategories()
+            article.Categories = GetAllCategories();
+            return View(article);
+        }
+
+        [NonAction]
+        public IEnumerable<SelectListItem> GetAllCategories()
+        {
+            // generam o lista goala
+            var selectList = new List<SelectListItem>();
+            // Extragem toate categoriile din baza de date
+            var categories = from cat in db.Categories select cat;
+            // iteram prin categorii
+            foreach (var category in categories)
+            {
+                // Adaugam in lista elementele necesare pentru dropdown
+                selectList.Add(new SelectListItem
+                {
+                    Value = category.CategoryId.ToString(),
+                    Text = category.CategoryName.ToString()
+                });
+            }
+            // returnam lista de categorii
+            return selectList;
+        }
+
+
+        [HttpPost]
+        public ActionResult New(Article article)
+        {
+            article.Categories = GetAllCategories();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Articles.Add(article);
+                    db.SaveChanges();
+                    TempData["message"] = "Articolul a fost adaugat!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(article);
+                }
+            }
+            catch (Exception e)
+            {
+                return View(article);
+            }
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+
+            Article article = db.Articles.Find(id);
+            ViewBag.Article = article;
+            article.Categories = GetAllCategories();
+            return View(article);
+        }
+
+
+        [HttpPut]
+        public ActionResult Edit(int id, Article requestArticle)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Article article = db.Articles.Find(id);
+                    if (TryUpdateModel(article))
+                    {
+                        article.Title = requestArticle.Title;
+                        article.Content = requestArticle.Content;
+                        article.Date = requestArticle.Date;
+                        article.CategoryId = requestArticle.CategoryId;
+                        db.SaveChanges();
+                        TempData["message"] = "Articolul a fost modificat!";
+                    }
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+
+            Article article = db.Articles.Find(id);
+            db.Articles.Remove(article);
+            db.SaveChanges();
+            TempData["message"] = "Articolul a fost sters!";
+            return RedirectToAction("Index");
+        }
+
+  
+    }
+
+}
+}
--- DAWProiect/Models/Article.cs
+++ DAWProiect/Models/Article.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace DAWProiect.Models
 {
@@ -10,6 +11,32 @@ namespace DAWProiect.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Titlu Stire")]
+        [StringLength(70, ErrorMessage = "Titlul este prea lung")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Continut Stire")]
+        [MinLength(10, ErrorMessage = "Stirea este prea scurta! Va rugam adaugati detalii!")]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Data Stire")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Poza Stire")]
+        public byte[] UserPhoto { get; set; }
+
         public string UserId { get; set; }
-        public virtual ApplicationUser User { get; set; }    }
+
+        public virtual ApplicationUser User { get; set; }
+
+        [Required(ErrorMessage = "Va rugam selectati categoria!")]
+        [Display(Name = "Categorie")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+    }
 }
